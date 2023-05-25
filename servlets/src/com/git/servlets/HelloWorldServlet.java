36ef1b00845a4b8822d798d484b0946d372e2fc8
Cristina Correa <ccorreas@minsait.com>
2023-05-25 09:37:37 +0200

    helloWorldServlet ok

--- servlets/src/com/git/servlets/HelloWorldServlet.java
+++ servlets/src/com/git/servlets/HelloWorldServlet.java
@@ -9,24 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorldServlet
- */
+
 @WebServlet("/helloWorld")
 public class HelloWorldServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public HelloWorldServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		// Step 1: set the content type
@@ -45,14 +38,6 @@ public class HelloWorldServlet extends HttpServlet {
 		out.println("</body></html>");
 	}
 
-	
-	
-	
-	
-	
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
